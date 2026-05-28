9cb14c2546509e4c8605cd8987910d2328f803b7
prestin liu <liuprestin@gmail.com>
2026-05-28 13:28:16 -0700

    remove global state and phase hook

--- runtime/phase_hooks_runtime.go
+++ runtime/phase_hooks_runtime.go
@@ -5,14 +5,9 @@ import (
 )
 
 // RegisterPhase1TickHook registers a phase-1 callback on this runtime.
-// A nil runtime falls back to the process-global hook registry for tests
-// and standalone timelines.
 func (rt *Runtime) RegisterPhase1TickHook(fn func(time.Duration)) func() {
 	if fn == nil {
 		return func() {}
-	}
-	if rt == nil {
-		return RegisterPhase1TickHook(fn)
 	}
 	rt.phase1HooksMu.Lock()
 	rt.phase1Hooks = append(rt.phase1Hooks, fn)
@@ -28,10 +23,6 @@ func (rt *Runtime) RegisterPhase1TickHook(fn func(time.Duration)) func() {
 }
 
 func (rt *Runtime) runPhase1TickHooks(dt time.Duration) {
-	if rt == nil {
-		runPhase1TickHooks(dt)
-		return
-	}
 	rt.phase1HooksMu.RLock()
 	hooks := append([]func(time.Duration){}, rt.phase1Hooks...)
 	rt.phase1HooksMu.RUnlock()
@@ -43,9 +34,6 @@ func (rt *Runtime) runPhase1TickHooks(dt time.Duration) {
 }
 
 func (rt *Runtime) clearPhase1TickHooks() {
-	if rt == nil {
-		return
-	}
 	rt.phase1HooksMu.Lock()
 	rt.phase1Hooks = nil
 	rt.phase1HooksMu.Unlock()
